f38250a3f9e1fd2bb5dcf2e904d247a08f2cb340
tempuses <tempuses@localhost>
2013-08-06 01:18:17 -0300

    interfaces de comunicacion para el webservice atenea

---
+++ src/org/squadra/atenea/ateneacommunication/AteneaWs.java
@@ -0,0 +1,27 @@
+package org.squadra.atenea.ateneacommunication;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import org.squadra.atenea.ateneacommunication.Message;
+
+/**
+ * Esta clase es el webservice que expone los metodos que se utilizan desde 
+ * AteneaClient
+ * 
+ * @author tempuses
+ */
+@WebService
+public interface AteneaWs {
+
+	/**
+	 * Metodo que permite dialogar con el servidor Atenea
+	 * @param message es el mensaje que va a enviar el cliente al servidor
+	 * @return mensaje de respuesta del servidor al cliente
+	 * @author tempuses
+	 */
+	
+	String dialogString( @WebParam(name="message")  String message );
+	
+	Message dialog( @WebParam(name="message")  Message message );
+}
